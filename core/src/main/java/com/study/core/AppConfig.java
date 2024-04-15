eddf6b8b1b3732b64e33926c846e562cbcac9023
sseohalee <seohalee0131@gmail.com>
2024-04-15 16:45:39 +0900

    fix: 스프링 컨테이너 적용

--- core/src/main/java/com/study/core/AppConfig.java
+++ core/src/main/java/com/study/core/AppConfig.java
@@ -2,28 +2,37 @@ package com.study.core;
 
 import com.study.core.discount.DiscountPolicy;
 import com.study.core.discount.FixDicountPolicy;
+import com.study.core.discount.RateDiscountPolicy;
 import com.study.core.member.MemberRepository;
 import com.study.core.member.MemberService;
 import com.study.core.member.MemberServiceImpl;
 import com.study.core.member.MemoryMemberRepository;
 import com.study.core.order.OrderService;
 import com.study.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션의 실제 동작에 필요한 "구현 객체"를 생성한다.
+// 설정 정보 어노테이션
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDicountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- core/src/main/java/com/study/core/MemberApp.java
+++ core/src/main/java/com/study/core/MemberApp.java
@@ -4,13 +4,19 @@ import com.study.core.member.Grade;
 import com.study.core.member.Member;
 import com.study.core.member.MemberService;
 import com.study.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // 회원 도메인 실행 예시(순수 자바) -> 테스트 이용
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // AppConfig -> Spring 변경
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
