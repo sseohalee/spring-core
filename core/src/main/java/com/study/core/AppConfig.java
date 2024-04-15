13a55bf7973321f593a024f313709a38cb31daac
sseohalee <seohalee0131@gmail.com>
2024-04-15 15:42:13 +0900

    refactor: AppConfig 리팩토링

--- core/src/main/java/com/study/core/AppConfig.java
+++ core/src/main/java/com/study/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.study.core;
 
+import com.study.core.discount.DiscountPolicy;
 import com.study.core.discount.FixDicountPolicy;
+import com.study.core.member.MemberRepository;
 import com.study.core.member.MemberService;
 import com.study.core.member.MemberServiceImpl;
 import com.study.core.member.MemoryMemberRepository;
@@ -10,9 +12,18 @@ import com.study.core.order.OrderServiceImpl;
 // 애플리케이션의 실제 동작에 필요한 "구현 객체"를 생성한다.
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDicountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDicountPolicy();
     }
 }
