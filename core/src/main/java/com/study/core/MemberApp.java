b88782508088ce014845570048fdec4a6c85194d
sseohalee <seohalee0131@gmail.com>
2024-04-15 09:46:08 +0900

    feat: 회원 도메인 테스트

---
+++ core/src/main/java/com/study/core/MemberApp.java
@@ -0,0 +1,19 @@
+package com.study.core;
+
+import com.study.core.member.Grade;
+import com.study.core.member.Member;
+import com.study.core.member.MemberService;
+import com.study.core.member.MemberServiceImpl;
+
+// 회원 도메인 실행 예시(순수 자바) -> 테스트 이용
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findeMember = memberService.findMEmber(1L);
+        System.out.println("new member = "+member.getName());
+        System.out.println("find member = "+findeMember.getName());
+    }
+}
---
+++ core/src/test/java/com/study/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package com.study.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+//import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMEmber(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
